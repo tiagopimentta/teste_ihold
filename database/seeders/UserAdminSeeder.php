95db4675be8b291d653bf7c896ff8271913456dd
Tiago Pimentta <tiagodrael@gmail.com>
2023-08-15 22:05:13 -0300

    Ajuste no seeds de comerciante

---
+++ database/seeders/UserAdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \DB::table('users')->insert(
+            [
+                'full_name' => 'Administrador',
+                'is_admin' => 1,
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' =>'adminssss',
+            ]
+        );
+    }
+}
