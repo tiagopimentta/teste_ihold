bc9e99a3edb0ab48720ea960a01313120825a690
Rodrigo Araujo <rodrigoluz@live.com>
2023-08-17 01:09:23 -0300

    arquivos de crud order

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,21 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreOrderRequest;
+use App\Http\Requests\UpdateOrderRequest;
 use App\Http\Resources\OrderResource;
 use App\Services\OrderService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
 
 class OrderController extends Controller
 {
     /**
      * @var OrderService
      */
-    protected OrderService $orderService;
+    protected OrderService $service;
 
-    public function __construct(OrderService $orderService)
+    public function __construct(OrderService $service)
     {
-        $this->orderService = $orderService;
+        $this->service = $service;
     }
 
 
@@ -29,20 +33,76 @@ class OrderController extends Controller
      *     @OA\Response(
      *         response=200,
      *         description="OK",
-     *
      *     ),
      *     security={{ "jwt": {} }}
      * )
      * @param Request $request
-     * @return AnonymousResourceCollection
+     * @return JsonResponse
      */
-    public function index(Request $request): AnonymousResourceCollection
+    public function index(Request $request): JsonResponse
     {
-        return OrderResource::collection(
+        return $this->ok(OrderResource::collection(
             $this
-                ->orderService
+                ->service
                 ->getRepository()
                 ->getPaginationList(params: $request->all(), with: ['items'])
-        );
+        ));
+    }
+
+    /**
+     * @param StoreOrderRequest $request
+     * @return JsonResponse
+     */
+    public function store(StoreOrderRequest $request): JsonResponse
+    {
+        try {
+            DB::beginTransaction();
+            $response = $this->service->save($request->all());
+            DB::commit();
+            return $this->success($this->messageSuccessDefault,
+                $response,
+                Response::HTTP_CREATED
+            );
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $this->error($e->getMessage());
+        }
+    }
+
+    /**
+     * @param UpdateOrderRequest $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function update(UpdateOrderRequest $request, int $id): JsonResponse
+    {
+        try {
+            DB::beginTransaction();
+            $response = $this->service->update($id, $request->all());
+            DB::commit();
+            return $this->success(
+                $this->messageSuccessDefault,
+                $response,
+                Response::HTTP_CREATED
+            );
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $this->error($e->getMessage());
+        }
+    }
+
+    /**
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function destroy(int $id): JsonResponse
+    {
+        try {
+            $this->service->getRepository()->find($id);
+            $this->service->delete($id);
+            return $this->success();
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage());
+        }
     }
 }
