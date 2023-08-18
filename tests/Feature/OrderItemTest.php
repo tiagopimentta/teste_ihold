70129519964167f25a01bbcbb1dd8a01e8abfb44
Rodrigo Araujo <rodrigoluz@live.com>
2023-08-18 00:12:13 -0300

    teste unitario com token auth login in head

--- tests/Feature/OrderItemTest.php
+++ tests/Feature/OrderItemTest.php
@@ -6,12 +6,19 @@ use Tests\TestCase;
 
 class OrderItemTest extends TestCase
 {
+    const BASE_API = 'api/orders/';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->logginUser();
+    }
     /**
      * A basic test example.
      */
     public function test_response_200_in_orders(): void
     {
-        $response = $this->get('/api/orders/1/items');
+        $response = $this->get(self::BASE_API . '1/items');
 
         $response->assertStatus(200);
     }
---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+
+    public function test_response_200_in_products_post(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_response_200_in_todos_products_get(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_response_200_in_unico_products_get(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_response_200_in_alterar_products_put(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_response_200_in_delete_products_delete(): void
+    {
+        $this->assertTrue(true);
+    }
+
+}
